cf57d58965abbc5bc972ee64ce2a2b1b6e5c2070
Nariman <mirmanov1@mail.ru>
2022-09-15 23:03:53 +0300

    feat: done lesson 59

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@
 
 // document.querySelector('h1').style.color = HEX
 
-// Стрелочные функции
+// 57 Стрелочные функции
 
 // function getAge(year) {
 //     const current = new Date().getFullYear()
@@ -43,12 +43,87 @@
 // logAge(1998)
 // console.log(getAge(2000))
 
-const person = {
-    age: 26,
-    firstName: "Nar",
-    logNameWithTimeOut() {
-        setTimeout(() => {
-            console.log(this.firstName);
-        }, 1000)
+// const person = {
+//     age: 26,
+//     firstName: "Nar",
+//     logNameWithTimeOut() {
+//         setTimeout(() => {
+//             console.log(this.firstName);
+//         }, 1000)
+//     }
+// } 
+
+// 58 
+// const createPost = (title, text = 'Default Txt', date = new Date().toLocaleDateString()) => {
+//     return {
+//         title,
+//         text, 
+//         date
+//     }
+// }
+
+// const post = createPost('New Year');
+// console.log(post);
+
+// 59 OBJ
+
+// const createCar = (name, model) => ({name, model})
+
+// const ford = createCar('Ford', 'Focus')
+// console.log(ford);
+
+// const yearField = 'year'
+
+// const BMW = {
+//     name: "BMW",
+//     ['model']: "X6",
+//     [yearField]: 2020,
+
+//     logFields() {
+//         const {name, model, year} = this
+//         console.log(name, model, year);
+//     }
+// }
+// console.log(BMW);
+// BMW.logFields()
+// const year = BMW.year
+
+// // const {year} = BMW
+// console.log(year);
+
+// 60 rest spread
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const title = form.title.value
+    const text = form.text.value
+    const desc = form.desc.value
+
+    saveForm({title, text, desc})
+})
+
+// Spread
+// function saveForm(data) {
+//     const formData = {
+//         date: new Date().toLocaleString(),
+//         ...data
+//     }
+
+//     console.log('Form data: ', formData);
+// }
+
+// rest
+function saveForm(...args) {
+    
+    const [title, text, desc] = args
+
+    const formData = {
+        date: new Date().toLocaleString(),
+        title, text, desc
     }
-} 
+
+    console.log('Form data: ', formData);
+}
